6bc1b4018e60146b7904e18b4c31445d628eff88
Moenga <danielmoenga17@gmail.com>
2023-03-25 20:50:48 +0300

    updated main.js
    Slider bug updated

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
 const testmContainer = document.querySelector(".testm-container");
@@ -28,37 +27,3 @@ rightArrow.addEventListener("click", () => {
     }
     moveToSlide(currentSlide);
 });
-
-// signInOut popup show handler code starts here
-
-const signIn = document.getElementById("sign-In");
-const signUp = document.getElementById("sign-Up");
-
-const goToSignUpButton = document.getElementById("goToSignUpButton");
-const returnToSignInButton = document.getElementById("returnToSignInButton");
-
-// const signPage = document.getElementById("sign-In");
-const signPopUp = document.getElementById("signPopUp");
-
-// display sign in page
-
-signPopUp.addEventListener("click", () => {
-    if (signUp.style.display = "none") {
-        signIn.style.display = "flex";
-    }
-
-});
-
-// toggle between sign in and sign up
-
-goToSignUpButton.addEventListener("click", () => {
-    signIn.style.display = "none";
-    signUp.style.display = "flex";
-});
-returnToSignInButton.addEventListener("click", () => {
-    signIn.style.display = "flex";
-    signUp.style.display = "none";
-});
-
-// signInOut popup show handler code ends here
-
