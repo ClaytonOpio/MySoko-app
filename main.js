424c67b007f2f0a4cb2bdd9cf9bb0656da473303
ClaytonOpio <claytonopio@gmail.com>
2023-03-25 10:55:11 +0300

    added soko.html

---
+++ main.js
@@ -0,0 +1,32 @@
+// signInOut popup show handler code starts here
+
+const signIn = document.getElementById("sign-In");
+const signUp = document.getElementById("sign-Up");
+
+const goToSignUpButton = document.getElementById("goToSignUpButton");
+const returnToSignInButton = document.getElementById("returnToSignInButton");
+
+// const signPage = document.getElementById("sign-In");
+const signPopUp = document.getElementById("signPopUp");
+
+// display sign in page
+
+signPopUp.addEventListener("click", () => {
+    if (signUp.style.display = "none") {
+        signIn.style.display = "flex";
+    }
+
+});
+
+// toggle between sign in and sign up
+
+goToSignUpButton.addEventListener("click", () => {
+    signIn.style.display = "none";
+    signUp.style.display = "flex";
+});
+returnToSignInButton.addEventListener("click", () => {
+    signIn.style.display = "flex";
+    signUp.style.display = "none";
+});
+
+// signInOut popup show handler code ends here
